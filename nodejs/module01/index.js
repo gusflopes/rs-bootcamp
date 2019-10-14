bfd65ad825273cac69498a7583bde0224fa1d50c
gusflopes <gusflopes86@gmail.com>
2019-10-14 14:15:52 -0400

    NodeJS: Basic Json Example

--- nodejs/module01/index.js
+++ nodejs/module01/index.js
@@ -1,33 +1,17 @@
 const express = require('express');
-//console.log(express);
 
 const server = express();
 
-server.get('/users/:id', (req, res) => {
-  const { id } = req.params.id;
+//Query params = ?teste=1
+//Route params = /users/1
+//Request body = { "name": "Diego", "email": "gusflopes86@gmail.com" }
 
-  return res.json({message: `Searching the user ${id}`});
+const users = ['Gustavo', 'Diego', 'Francisco'];
+
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
 });
-
-/* #### FIRST THINGS
-// Query params = ?teste=1
-// Route params = /users/1
-// Request body = {"name": "Gustavo", "email": "gusflopes86@gmail.com"}
-// Request headers = payload, etc
-
-server.get('/teste', (req, res) => {
-  console.log('Mensagem no Console do Servidor');
-
-  const name = req.query.name;
-
-  //return res.send('Hello World');
-  //return res.json({ message: 'Hello World' });
-
-  //Request for this: http://localhost:3000/teste?name=Gustavo
-  return res.json({ message: `Hello ${name}!` });
-});
-
-*/
-
 
 server.listen(3000);
